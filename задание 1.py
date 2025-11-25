c13c26b8c4fc7db71c1e0f307b5391136c03b97c
uzukamiaoi-lang <uzukamiaoi@gmail.com>
2025-11-25 20:41:49 +0300

    Add files via upload

---
+++ задание 1.py
@@ -0,0 +1,127 @@
+# #1
+# import math
+# x=int(input())
+# y=3*x**2+math.sin(x+2)
+# print(y)
+
+
+#2
+# import math
+# x=int(input())
+# a=int(input())
+# y=a*x**2+math.cos(2*x+1)
+# print(y)
+
+#3
+# x,a,b=map(int,input().split())
+# y=a*x+b*math.sin(2*x+2)
+# print(y)
+
+
+# #4
+# x,a=map(int,input().split())
+# y=a*x**3+math.cos(3*x+1)
+# print(y)
+
+
+#5
+# x,a=map(int,input().split())
+# y=x**2/a+math.cos(2*x-1)
+# print(y)
+
+#6
+# x,a=map(int,input().split())
+# y=x/a+2*x**2
+# print(y)
+
+#7
+# x=int(input())
+# y=3*x**2-2*x+1
+# print(y)
+
+# 8
+# x=int(input())
+# y=0.5*x**2-3*x+1
+# print(y)
+
+
+#9
+# x,a=map(int,input().split())
+# y=1/(x**2+1)-a
+# print(y)
+
+#10
+# x,a=map(int,input().split())
+# y=a/(x**2+1)-math.cos(2*x-1)
+# print(y)
+
+#11
+# x=int(input())
+# y=x**3-2*x**2+4
+# print(y)
+
+
+#12
+# x,a,b=map(int,input().split())
+# y=a*x**2+b*x**3-8
+# print(y)
+
+
+#13
+# x,a,b=map(int,input().split())
+# y=a*(math.sqrt(x**2+4))-b
+# print(y)
+
+#14
+# x=int(input())
+# y=math.cos(2*x-1)+math.sin(x)
+# print(y)
+
+# 15
+# x,a,b=map(int,input().split())
+# y=a*math.sqrt(x)+b*x**2
+# print(y)
+
+# БЛОК2
+#1
+# a,b,c=map(int,input().split())
+# V=a*b*c
+# S=2*(a*b+b*c+a*c)
+# print(V,S)
+
+
+# 2
+# R=int(input())
+# L=2*math.pi*R
+# S=math.pi*R**2
+# print(L,S)
+
+#3
+# a, b = map(int, input().split())
+# c = math.sqrt(a ** 2 + b ** 2)
+# P = a + b + c
+# print(c, P)
+
+
+#4
+# R1,R2=map(int,input().split())
+# S1=math.pi*R1**2
+# S2=math.pi*R2**2
+# S3=S1-S2
+# print(S3)
+
+#5
+# x1,y1,x2,y2=map(int,input().split())
+# st1=x2-x1
+# st2=y2-y1
+# S=st1*st2
+# P=2*(st1+st2)
+# print(S,P)
+
+#6
+#x1,y1,x2,y2=map(int,input().split())
+#f=math.sqrt((x2-x1)**2+(y2-y1)**2)
+#print(f)
+
+#7
+#x1,y1,x2,y2,x3,y3=map(int,input().split())
