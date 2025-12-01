33b75ac866453d0f71fc1e1bdfd99e771e6de687
uzukamiaoi-lang <uzukamiaoi@gmail.com>
2025-12-01 23:00:03 +0300

    Add files via upload

---
+++ задание 7.py
@@ -0,0 +1,78 @@
+#1
+#lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  
+#odd_numbers = [x for x in lst if x % 2 != 0]
+#odd_numbers.sort(reverse=True)
+#print(odd_numbers)
+#2
+#lst = [-3, -1, 0, 2, 5, 8, -7, 10]  
+#positive_numbers = [x for x in lst if x > 0]
+#positive_numbers.sort()
+#print(positive_numbers)
+#3
+#lst = [-3, -1, 0, 2, 5, 8, -7, 10]  
+#positive_numbers = [x for x in lst if x > 0]
+#positive_numbers.sort(reverse=True)
+#print(positive_numbers)
+#4
+#lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  
+#even_numbers = [x for x in lst if x % 2 == 0]
+#even_numbers.sort()
+#print(even_numbers)
+#5
+#lst = [1, 3, 5, 7, 9, 2, 8]  
+#k = 5  # замените на нужное число
+#greater_than_k = [x for x in lst if x > k]
+#greater_than_k.sort(reverse=True)
+#print(greater_than_k)
+#6
+#lst = [5, 12, 8, 15, 3, 20, 7]  
+#greater_than_10 = [x for x in lst if x > 10]
+#greater_than_10.sort()
+#print(greater_than_10)
+#7
+#lst = [5, 10, 3, 15, 8, 20, 7]  
+#multiple_of_5 = [x for x in lst if x % 5 == 0]
+#multiple_of_5.sort(reverse=True)
+#print(multiple_of_5)
+#8
+#lst = [1, 3, 5, 7, 9, 2, 8]  
+#k = 7  
+#less_than_k = [x for x in lst if x < k]
+#less_than_k.sort()
+#print(less_than_k)
+#9
+#lst = [10, 12, 18, 5, 14, 20, 3]  
+#less_than_15 = [x for x in lst if x < 15]
+#less_than_15.sort(reverse=True)
+#print(less_than_15)
+#10
+#lst = [3, 6, 9, 12, 15, 4, 7]  
+#multiple_of_3 = [x for x in lst if x % 3 == 0]
+#multiple_of_3.sort()
+#print(multiple_of_3)
+#11
+#lst = [4, 8, 12, 16, 5, 9, 20]  
+#k = 4  
+#multiple_of_k = [x for x in lst if x % k == 0]
+#multiple_of_k.sort(reverse=True)
+#print(multiple_of_k)
+#12
+#lst = [-3, -1, 0, 2, -5, 8, -7]  
+#negative_numbers = [x for x in lst if x < 0]
+#negative_numbers.sort()
+#print(negative_numbers)
+#13
+#lst = [10, 20, 30, 40, 50, 60] 
+#odd_index_numbers = [lst[i] for i in range(1, len(lst), 2)]
+#odd_index_numbers.sort(reverse=True)
+#print(odd_index_numbers)
+#14
+#lst = [5, 15, 99, 3, 25, 100, 45]  
+#two_digit_numbers = [x for x in lst if 10 <= abs(x) <= 99]
+#two_digit_numbers.sort()
+#print(two_digit_numbers)
+#15
+#lst = [10, 20, 30, 40, 50, 60]  
+#even_index_numbers = [lst[i] for i in range(0, len(lst), 2)]
+#even_index_numbers.sort()
+#print(even_index_numbers)
