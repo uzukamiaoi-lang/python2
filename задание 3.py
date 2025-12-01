ffcfc1cb82be0e6230e7affb550fed18ef06fb47
uzukamiaoi-lang <uzukamiaoi@gmail.com>
2025-12-01 22:57:03 +0300

    Add files via upload

---
+++ задание 3.py
@@ -0,0 +1,127 @@
+#1
+# a, x = map(int, input().split())
+# if x > 0:
+#     y = a * x ** 2 + 1
+# else:
+#     y = a * x - 1
+# print(y)
+
+
+#2
+# a, x = map(int, input().split())
+# if x >= 1:
+#     y = a * x + 1
+# else:
+#     x ** 2 - 1
+# print(y)
+
+
+#3
+# a, x = map(int, input().split())
+# if x < 0:
+#     y = 3 * a ** 2
+# else:
+#     y = 4 * a * x - 1
+# print(y)
+
+
+# #4
+# a, x = map(int, input().split())
+# if x > 4:
+#     y = 2 * a ** 2
+# else:
+#     y = 3 * x - 1
+# print(y)
+
+
+#5
+# a, x = map(int, input().split())
+# if x > 2:
+#     y = 2 * a * x - 2
+# else:
+#     y = 3 * a ** 2 - 2 * x
+# print(y)
+
+
+#6
+# a, x = map(int, input().split())
+# if x > 1:
+#     y = 2 * a * x ** 2 - 1
+# else:
+#     y = x
+# print(y)
+
+#7
+# a, x = map(int, input().split())
+# if x > 2:
+#     y = x ** 2
+# else:
+#     y = 2 * a - 1
+# print(y)
+
+#8
+# x = int(input())
+# if x > 2:
+#     y = math.cos(2 * x - 1)
+# else:
+#     y = math.sin(3 * x + 1)
+# print(y)
+
+#9
+# x = int(input())
+# if x > 2:
+#     y = 2 * x ** 3 - 2 * x - 1
+# else:
+#     y = 3 * x ** 2 - 2 * x + 1
+# print(y)
+
+#10
+# a, x = map(int, input().split())
+# if x > 1:
+#     y = 2 * a * x ** 2 - 1
+# else:
+#     y = 1 / a
+# print(y)
+
+
+#11
+# a, x = map(int, input().split())
+# if x >= 0:
+#     y = a * math.sqrt(x) + 1
+# else:
+#     y = a * x - 1
+# print(y)
+
+
+# #12
+# a, x = map(int, input().split())
+# if x >= 0:
+#     y = math.sqrt(x) + a
+# else:
+#     y = a / x - 1
+# print(y)
+
+#13
+# a, x = map(int, input().split())
+# if x > 0:
+#     y = 1 / x + a
+# else:
+#     y = x ** 2 - 1
+# print(y)
+
+
+#14
+# x = int(input())
+# if x > math.pi / 2:
+#     y = math.cos(x)
+# else:
+#     y = math.sin(x)
+# print(y)
+
+#15
+# x = int(input())
+# if x > 2:
+#     y = math.sqrt(x - 2)
+# else:
+#     y = (x - 2) ** 2 + 1
+# print(y)
